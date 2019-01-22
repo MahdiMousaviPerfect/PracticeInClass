3d3232a3a0d4455a552e9d27acb83f4f85d7bd7f
SMahdiMousaviPerfect <m.mousavi@ikco.ir>
2019-01-22 10:17:30 +0330

    Add MessageBox class in Project

---
+++ ConsoleApplication1/MessageBox.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace ConsoleApplication1
+{
+    class MessageBox
+    {
+        public MessageBox()
+        {
+
+        }
+
+        public string result { get; set; }
+
+        public virtual void Message()
+        {
+            
+        }
+    }
+}

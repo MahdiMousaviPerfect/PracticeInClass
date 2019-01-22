7485bf505e1a2d049b008dd73a6a44972eabdbba
SMahdiMousaviPerfect <m.mousavi@ikco.ir>
2019-01-22 10:55:36 +0330

    Add Programs.cs class in Project

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            DatesConverter dc = new DatesConverter(2019, 01, 22);// روز سه شنبه مورخ 2 بهمن 1397
+            dc.MiladiToShamsi();
+            System.Console.WriteLine(System.Environment.NewLine);
+            dc.MiladiToGhamari();
+            System.Console.WriteLine(System.Environment.NewLine);
+            dc.MiladiToHebrew();
+            System.Console.WriteLine(System.Environment.NewLine);
+            dc.MiladiToGregorian();
+            System.Console.WriteLine(System.Environment.NewLine);
+            System.Console.Write("Press [Enter] To Exit Program!");
+            System.Console.ReadLine();
+            // end/end
+        }
+    }
+}
